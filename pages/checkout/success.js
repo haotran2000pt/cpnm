4c48759f65cb2e6e8ea38a1988dbc87dde9e7995
18110103 <79066729+haotran2000pt@users.noreply.github.com>
2021-07-06 18:55:48 +0700

    Update success.js

--- pages/checkout/success.js
+++ pages/checkout/success.js
@@ -1,15 +1,14 @@
 export async function getServerSideProps({ req }) {
     const headers = req.headers
     return {
-        props: { headers, req }
+        props: { headers }
     }
 }
 
-export default function SuccessPage({ headers, req }) {
+export default function SuccessPage({ headers }) {
     return (
         <div>
             <button onClick={() => console.log(headers)}>headers</button>
-            <button onClick={() => console.log(req)}>req</button>
         </div>
     )
 }
